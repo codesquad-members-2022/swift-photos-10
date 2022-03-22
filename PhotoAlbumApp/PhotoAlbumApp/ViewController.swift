80078dabcfa23b89388559e1c583fd02f2ffde7b
Song TaeHwan <axoghks@gmail.com>
2022-03-22 19:36:41 +0900

    [Feat] 컬렉션 뷰 및 랜덤한 배경색을 가진 셀 40개 추가

--- PhotoAlbumApp/PhotoAlbumApp/ViewController.swift
+++ PhotoAlbumApp/PhotoAlbumApp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let reusableCellName = "photoItem"
+    private let cellCount = 40
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureCollectionView()
     }
-
-
+    
+    private func configureCollectionView() {
+        self.collectionView.frame = self.view.bounds
+        self.collectionView.dataSource = self
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.cellCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.reusableCellName, for: indexPath)
+        cell.backgroundColor = .random()
+        cell.frame.size = CGSize(width: 100, height: 100)
+        
+        return cell
+    }
+}
