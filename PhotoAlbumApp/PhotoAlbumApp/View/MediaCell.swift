4bec726e729bd4a04ea67009c6c1b1cd8d8df23d
Song TaeHwan <axoghks@gmail.com>
2022-03-28 17:48:37 +0900

    [Feat] 커스텀 컬렉션 뷰 셀 추가

--- PhotoAlbumApp/PhotoAlbumApp/View/MediaCell.swift
+++ PhotoAlbumApp/PhotoAlbumApp/View/MediaCell.swift
@@ -5,4 +5,40 @@
 //  Created by 송태환 on 2022/03/28.
 //
 
-import Foundation
+import UIKit
+
+class MediaCell: UICollectionViewCell {
+    private let imageView = UIImageView(frame: .zero)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configure()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+    }
+    
+    private func configure() {
+        self.contentView.addSubview(imageView)
+        self.imageView.frame = self.bounds
+        self.imageView.autoresizingMask = [
+            .flexibleWidth,
+            .flexibleHeight,
+            .flexibleTopMargin,
+            .flexibleLeftMargin,
+            .flexibleRightMargin,
+            .flexibleBottomMargin
+        ]
+    }
+    
+    func setImage(_ image: UIImage) {
+        self.imageView.image = image
+    }
+}
